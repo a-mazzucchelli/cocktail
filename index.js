54e12909d28ac258473e7fa15c8bca4d75eee535
Daniela Calore <danielacalore72@gmail.com>
2020-07-06 15:49:24 +0000

    added home routing

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const path = require("path")
 const app = express();
-
 const port = 3000;
+
+app.get('/',(req, res) =>{
+  res.redirect ("/home")
+})
+
+app.get('/home',(req, res) =>{
+  res.sendFile (path.join(__dirname, "/home.html"))
+})
 
 app.get('/random',(req, res) =>{
   res.sendFile (path.join(__dirname, "/random.html"))
 })
 
 app.get('*',(req, res) =>{
-  res.send('404 not found')
+  res.sendFile (path.join(__dirname, "/404.html"))
 })
 
 app.listen(port,() => console.log(`ascolta porta ${port}`));
