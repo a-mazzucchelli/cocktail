05d258a836c59b9f744bff96e1faad37250506df
Luca Bettio <lucabett92@gmail.com>
2020-07-07 10:32:43 +0000

    add better routing

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/random', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/random.html'));
 });
 
-app.get('/cocktail/:idCocktail', (req, res) => {
+app.get('/cocktail', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/cocktail.html'));
 })
 
