fcd85fa57eb64f95e396097d45c5027f66c8297d
Alessandro Mazzucchelli <mazzucchelli.work@gmail.com>
2020-07-07 07:14:03 +0000

    organized files

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const path = require("path")
+const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/',(req, res) =>{
-  res.redirect ("/home")
-})
+app.get('/', (req, res) => {
+  res.redirect('/home');
+});
 
-app.get('/home',(req, res) =>{
-  res.sendFile (path.join(__dirname, "/home.html"))
-})
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/home.html'));
+});
 
-app.get('/random',(req, res) =>{
-  res.sendFile (path.join(__dirname, "/random.html"))
-})
+app.get('/random', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/random.html'));
+});
 
-app.get('*',(req, res) =>{
-  res.sendFile (path.join(__dirname, "/404.html"))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/404.html'));
+});
 
-app.listen(port,() => console.log(`ascolta porta ${port}`));
+app.listen(port, () => console.log(`App listening on port ${port}`));
