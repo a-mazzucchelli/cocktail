69f974c37fbdcb08e85595be63e1a6d012a38fd6
Alessandro Mazzucchelli <mazzucchelli.work@gmail.com>
2020-07-08 07:42:59 +0000

    added style pages

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.redirect('/home');
 });
@@ -17,7 +19,7 @@ app.get('/random', (req, res) => {
 
 app.get('/cocktail', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/cocktail.html'));
-})
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/404.html'));
