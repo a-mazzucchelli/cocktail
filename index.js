1de16d2439347460ccb4488ea6ac1ca02da7509e
Alessandro Mazzucchelli <mazzucchelli.work@gmail.com>
2020-07-10 10:59:09 +0000

    added api route

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+const { response } = require('express');
 const app = express();
 const port = 3000;
 
@@ -25,9 +27,35 @@ app.get('/favourites', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/favourites.html'));
 });
 
-// app.get('/cardview', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/views/cardview.html'));
-// })
+app.get('/api/searchByIngredients', (req, res) => {
+  const result = { drinks: [] };
+
+  const ingredient = req.query.ingredient;
+
+  axios
+    .get(
+      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
+    )
+    .then((response) => {
+      const drinksByIngredient = response.data.drinks;
+      let responseCount = 0;
+      drinksByIngredient.forEach((drink) => {
+        result.drinks.push(drink.strDrink);
+        responseCount++;
+
+        if (responseCount === drinksByIngredient.length) {
+          res.json(result);
+        }
+      });
+    });
+
+  // const ingrArray = req.query.ingredients.split(',');
+  // console.log(ingrArray);
+
+  // ingrArray.forEach(ingredient => {
+  //   call to https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=ingredient
+  // })
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/404.html'));
